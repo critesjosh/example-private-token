36ab0e7d6743692cfd6aa709071b07bcce9d6799
Josh Crites <jc@joshcrites.com>
2023-09-08 16:14:11 -0400

    init

---
+++ README.md
@@ -0,0 +1,57 @@
+# Example Project Walkthrough
+
+- importing Aztec.nr in Nargo.toml
+
+## Compiling contracts
+
+### Requirements
+
+#### nargo
+
+[Installation guide](https://noir-lang.org/getting_started/nargo_installation#installation)
+
+#### aztec-cli
+
+```bash
+yarn global add @aztec/cli
+```
+
+### Basic
+
+```bash
+aztec-cli compile ./path/to/my_aztec_contract_project
+```
+
+### With Typescript interfaces
+
+```bash
+aztec-cli compile --typescript . .
+```
+
+### With Noir interfaces
+
+```bash
+aztec-cli compile --interface ./path/to/another_aztec_contract_project/src ./path/to/my_aztec_contract_project
+```
+
+## Deploying
+
+The example contract takes 2 args, an amount and an address.
+
+```bash
+aztec-cli deploy target/Example.json --args 1000000 $ADDRESS
+```
+
+## Testing
+
+Start an Ethereum dev net with
+
+```bash
+anvil
+```
+
+Start the Sandbox with
+
+```bash
+npx @aztec/aztec-sandbox
+```
---
+++ tests/Example.test.ts
@@ -0,0 +1,42 @@
+import { AztecRPC, AccountWallet, createAztecRpcClient, getSchnorrAccount, waitForSandbox, Wallet } from '@aztec/aztec.js';
+import { GrumpkinScalar } from '@aztec/circuits.js';
+import { ExampleContract } from '../Example.js';
+
+const { SANDBOX_URL = 'http://localhost:8080' } = process.env;
+
+let rpc: AztecRPC;
+let account: AccountWallet;
+let exampleContract: ExampleContract;
+
+describe('Testing Example Aztec contract', () => {
+
+    beforeEach(async () => {
+        rpc = createAztecRpcClient(SANDBOX_URL);
+        // await waitForSandbox(rpc);
+        // [account] = await getSandboxAccountsWallets(rpc);
+
+    });
+
+    afterEach(() => {
+
+    })
+
+    it('does a test', async () => {
+        account = await getSchnorrAccount(rpc, GrumpkinScalar.random(), GrumpkinScalar.random()).waitDeploy();
+        expect(account).toBeDefined();
+    })
+
+    it('Deploys a contract', async () => {
+        const address = await account.getAddress();
+        const exampleContract = await ExampleContract.deploy(rpc, 100n, address).send().deployed();
+        expect(exampleContract).toBeDefined();
+    })
+
+    it('Transfers a token', () => {
+
+    })
+
+})
+
+
+
